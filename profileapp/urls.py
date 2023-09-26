9786feacd53ab9edb1c860c5c7409193c973d4d3
saurabh <88712236+codewithsaurabh9@users.noreply.github.com>
2023-09-26 14:07:49 +0530

    jjj

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,21 +1,20 @@
 from xml.dom.minidom import Document
-from django.conf import settings
+# from django.conf import settings
 from django.urls import path
-from django.urls import path,include
-from django.conf.urls.static import static
+from django.urls import path, include
+# from django.conf.urls.static import static
 
-from.import views
+from .import views
 
 # from django.conf.urls import url
 
 
-
 urlpatterns = [
     path('', views.index, name='index'),
-    path(r'foo/',views.download),
+    path(r'foo/', views.download),
 
 
 
 
 
-    ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
